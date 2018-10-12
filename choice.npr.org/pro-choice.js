e2c6758847de87dbb91694058b79d6f994b97b27
Jakub Wilk <jwilk@jwilk.net>
2018-10-12 21:27:40 +0200

    choice.npr.org: refactor.

--- choice.npr.org/pro-choice.js
+++ choice.npr.org/pro-choice.js
@@ -1,3 +1,5 @@
-document.querySelector('#textLink').href = 'https://text.npr.org/s.php?sId=' + window.location.search.match('[0-9]{9,}');
+let id = window.location.search.match('[0-9]{9,}');
+let a = document.querySelector('#textLink')
+a.href = `https://text.npr.org/s.php?sId=${id}`;
 
 // vim:ts=4 sts=4 sw=4 et
