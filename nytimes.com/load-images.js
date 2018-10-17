c9b1c3fac62fc2825362e36adfab389fe07d0f6c
Jakub Wilk <jwilk@jwilk.net>
2018-10-17 13:49:51 +0200

    nytimes.com: add.

---
+++ nytimes.com/load-images.js
@@ -0,0 +1,16 @@
+let figures = document.querySelectorAll('figure[itemprop="associatedMedia"]');
+for (let figure of figures)
+{
+    let img = figure.querySelector('img');
+    if (img === null) {
+        let div = figure.querySelector('div');
+        if (div !== null) {
+            img = document.createElement('img');
+            img.src = figure.getAttribute('itemid');
+            div.replaceWith(img);
+        }
+    }
+}
+
+
+// vim:ts=4 sts=4 sw=4 et
