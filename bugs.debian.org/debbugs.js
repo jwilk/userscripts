68c40fe21f0caedfc31fb9445984b56020cd8529
Jakub Wilk <jwilk@jwilk.net>
2019-08-23 19:58:54 +0200

    bugs.debian.org: hide libravatar images.

--- bugs.debian.org/debbugs.js
+++ bugs.debian.org/debbugs.js
@@ -1,6 +1,7 @@
 let elts = document.querySelectorAll('img[src^="/cgi-bin/libravatar.cgi?"]');
 for (let p of elts) {
     p.src = 'data:text/plain,';
+    p.style.display = 'none';
 }
 
 // vim:ts=4 sts=4 sw=4 et
