04e196918c8cc41cc4981ef54d374f140d5c2814
Jakub Wilk <jwilk@jwilk.net>
2020-07-06 06:07:02 +0200

    gizmodo.com: fix loading images.

--- gizmodo.com/load-images.js
+++ gizmodo.com/load-images.js
@@ -1,8 +1,11 @@
-let lazy_sources = document.querySelectorAll('source[data-srcset]');
-for (let source of lazy_sources) {
-    if (source.media)
-        continue;
-    source.srcset = source.dataset.srcset;
+let imgs = document.querySelectorAll('img[data-srcset]');
+for (let img of imgs) {
+    let picture = document.createElement('picture');
+    img.parentNode.replaceChild(picture, img);
+    let source = document.createElement('source');
+    source.srcset = img.dataset.srcset;
+    picture.appendChild(source);
+    picture.appendChild(img);
 }
 
 // vim:ts=4 sts=4 sw=4 et
