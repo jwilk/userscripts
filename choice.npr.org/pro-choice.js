5a29fd5573e759b70881e895311f065cf41c7f8c
Jakub Wilk <jwilk@jwilk.net>
2020-12-23 22:29:44 +0100

    choice.npr.org: fix button label.

--- choice.npr.org/pro-choice.js
+++ choice.npr.org/pro-choice.js
@@ -1,5 +1,6 @@
 let id = window.location.search.match('[0-9]{8,}');
 let a = document.querySelector('#textLink')
 a.href = `https://text.npr.org/${id}`;
+a.text = 'go to text-only version';
 
 // vim:ts=4 sts=4 sw=4 et
