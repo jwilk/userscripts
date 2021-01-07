9e829eb437a38ed2d5fbc79af092811b16cc423b
Jakub Wilk <jwilk@jwilk.net>
2021-01-07 18:11:35 +0100

    sowa.pl: add more JS bits.

--- sowa.pl/sowa.js
+++ sowa.pl/sowa.js
@@ -4,5 +4,18 @@ for (let elt of elts) {
         elt.href = '/sowacgi.php?KatID=0&typ=record&001=' + elt.dataset['001']
     }
 }
+elts = document.querySelectorAll('div[onclick]');
+for (let elt of elts) {
+    let action = elt.getAttribute('onclick');
+    let match = action.match("^location[.]href='(https:.*)'$");
+    if (match !== null) {
+        let url = match[1];
+        let a = document.createElement('a');
+        a.href = url;
+        elt.parentNode.replaceChild(a, elt);
+        elt.removeAttribute('onclick');
+        a.appendChild(elt);
+    }
+}
 
 // vim:ts=4 sts=4 sw=4 et
