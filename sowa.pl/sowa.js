758e59501b5819fa1a52a001e53e99824afcfdf1
Jakub Wilk <jwilk@jwilk.net>
2021-01-19 16:50:14 +0100

    sowa.pl: add more JS bits.

--- sowa.pl/sowa.js
+++ sowa.pl/sowa.js
@@ -17,5 +17,14 @@ for (let elt of elts) {
         a.appendChild(elt);
     }
 }
+elts = document.querySelectorAll('a[href^="javascript:Konto("]');
+for (let elt of elts) {
+    let url = elt.href;
+    let match = url.match("^javascript:Konto[(]0\\s*,'(\\w+)',\\s*'',\\s*''[)];$");
+    if (match !== null) {
+        let id = match[1];
+        elt.href = `/sowacgi.php?KatID=0&typ=acc&id=${id}`;
+    }
+}
 
 // vim:ts=4 sts=4 sw=4 et
