6d677451633f0679d3747e2d0eeebea47d2c7f2d
Jakub Wilk <jwilk@jwilk.net>
2021-02-16 09:43:02 +0100

    imdb.com: load images.

--- imdb.com/imdb.js
+++ imdb.com/imdb.js
@@ -2,5 +2,11 @@ let elts = document.querySelectorAll('a[href*="ref_="]');
 for (let a of elts) {
     a.href = a.href.replace(/[?]ref_=[^&]+/, "");
 }
+let imgs = document.querySelectorAll('img.loadlate');
+for (let img of imgs) {
+    console.log(img);
+    img.src = img.getAttribute('loadlate');
+    img.classList.remove('hidden');
+}
 
 // vim:ts=4 sts=4 sw=4 et
