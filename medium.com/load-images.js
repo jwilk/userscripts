812770f3907f9b348ab2e28a74c47244ebe3e72e
Jakub Wilk <jwilk@jwilk.net>
2021-04-03 11:39:43 +0200

    medium.com: delete obsolete code.

--- medium.com/load-images.js
+++
@@ -1,8 +0,0 @@
-let lazy_images = document.querySelectorAll('img.progressiveMedia-image');
-for (let img of lazy_images) {
-    let wd = img.parentElement.offsetWidth;
-    img.src = img.dataset.src;
-    img.className = 'progressiveMedia-noscript';
-}
-
-// vim:ts=4 sts=4 sw=4 et
