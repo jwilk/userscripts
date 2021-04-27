724470a16a386f9af4dd2a1bb66cdab989d9433c
Jakub Wilk <jwilk@jwilk.net>
2021-04-27 18:26:42 +0200

    consent.google.com: disable the “I agree” button.

--- consent.google.com/google-consent.js
+++ consent.google.com/google-consent.js
@@ -2,5 +2,10 @@ let elts = document.querySelectorAll('input[name^="set_"][value="false"]');
 for (let elt of elts) {
     elt.click();
 }
+let elt = document.querySelector('input[type="submit"][value="I agree"]');
+if (elt !== null) {
+    console.log(elt);
+    elt.disabled = true;
+}
 
 // vim:ts=4 sts=4 sw=4 et
