9d0ddd67824dda8166c2c02cf246e60c411b1bd2
Jakub Wilk <jwilk@jwilk.net>
2022-06-20 20:54:05 +0200

    bloomberg.com: load more images.

--- bloomberg.com/load-images.js
+++ bloomberg.com/load-images.js
@@ -3,5 +3,12 @@ for (let img of lazy_images) {
     img.src = img.dataset.nativeSrc;
     img.classList.add('loaded');
 }
+lazy_images = document.querySelectorAll('img.dvz-lazy');
+for (let img of lazy_images) {
+    let widths = JSON.parse(img.dataset.widths);
+    let pattern = img.dataset.pattern;
+    let srcset = widths.map(wd => pattern.replace('{{size}}', wd) + ` ${wd}w`);
+    img.srcset = srcset.join(', ');
+}
 
 // vim:ts=4 sts=4 sw=4 et
