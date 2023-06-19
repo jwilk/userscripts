dcfb144f83f884958b9cda3445e4651d0e223712
Jakub Wilk <jwilk@jwilk.net>
2023-06-19 17:00:54 +0200

    hekko.pl: remove.

--- hekko.pl/hekko.js
+++
@@ -1,9 +0,0 @@
-let elts = document.querySelectorAll('p.nbswitch');
-for (let p of elts) {
-    if (p.style.display == 'none') {
-        p.style.removeProperty('display');
-        p.style.opacity = 0.5;
-    }
-}
-
-// vim:ts=4 sts=4 sw=4 et
