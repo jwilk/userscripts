fc45eccc97d66689834579d88057e332fadbb0c5
Jakub Wilk <jwilk@jwilk.net>
2024-07-16 18:17:27 +0200

    bugs.debian.org: rename var.

--- bugs.debian.org/debbugs.js
+++ bugs.debian.org/debbugs.js
@@ -1,7 +1,7 @@
 let elts = document.querySelectorAll('img[src^="/cgi-bin/libravatar.cgi?"]');
-for (let p of elts) {
-    p.src = 'data:text/plain,';
-    p.style.display = 'none';
+for (let elt of elts) {
+    elt.src = 'data:text/plain,';
+    elt.style.display = 'none';
 }
 
 // vim:ts=4 sts=4 sw=4 et
