6a39be0a3c31033a1d920668bb7d954e403d1ad4
Jakub Wilk <jwilk@jwilk.net>
2024-08-09 08:46:05 +0200

    gocomics.com: load images.

---
+++ gocomics.com/load-images.js
@@ -0,0 +1,7 @@
+let lazy_images = document.querySelectorAll('img.lazyload');
+for (let img of lazy_images) {
+    img.srcset = img.dataset.srcset;
+    img.style.opacity = 1;
+}
+
+// vim:ts=4 sts=4 sw=4 et
