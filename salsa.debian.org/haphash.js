5d351065d18fce859e4b6f8f967d2ecba9b3b761
Jakub Wilk <jwilk@jwilk.net>
2026-02-14 18:38:17 +0100

    salsa.debian.org/haphash: add.

---
+++ salsa.debian.org/haphash.js
@@ -0,0 +1,33 @@
+async function solve(progress) {
+    const challenge = document.cookie.match(/pow_challenge=([^;]+)/)[1];
+    const params = new URLSearchParams(location.search);
+    const orig_url = params.get('original') || '/';
+    const encoder = new TextEncoder()
+    for (let i = 0; ; i++) {
+        progress.value = i;
+        progress.max = i + 0xFFFF;
+        const message = `${challenge};${i}`;
+        const bytes = encoder.encode(message);
+        const hash = await window.crypto.subtle.digest('SHA-256', bytes);
+        const ahash = new Uint8Array(hash);
+        if (ahash[0] || ahash[1])
+            continue;
+        document.cookie = `pow_nonce=${i}; path=/`;
+        location.href = orig_url;
+        return;
+    }
+}
+
+if (location.pathname == '/challenge.html') {
+    let progress = document.createElement('progress');
+    progress.style.width = '100%';
+    progress.value = 0;
+    progress.max = 0xFFFF;
+    document.body.appendChild(progress);
+    let button = document.createElement('button');
+    button.textContent = 'Challenge accepted!';
+    button.onclick = function() { solve(progress); };
+    document.body.appendChild(button);
+}
+
+// vim:ts=4 sts=4 sw=4 et
