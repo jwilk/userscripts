e7789211da617a5b49f9adc94184032d746d6999
Jakub Wilk <jwilk@jwilk.net>
2026-03-18 10:07:48 +0100

    aldi.pl/ipaper: add.

---
+++ aldi.pl/ipaper.js
@@ -0,0 +1,13 @@
+let div = document.querySelector('div.IPaper');
+if (div !== null && div.childElementCount == 0) {
+    let params = div.dataset.params;
+    params = JSON.parse(params);
+    let paper_url = params.paperUrl;
+    paper_url += '/GetPDF.ashx';
+    let link = document.createElement('a');
+    link.href = paper_url;
+    link.text = '⤓ PDF'
+    div.appendChild(link);
+}
+
+/* vim:set ts=4 sts=4 sw=4 et: */
