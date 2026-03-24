40e253463ddd046ffc8aef10b1306c6376923d59
Jakub Wilk <jwilk@jwilk.net>
2026-03-24 21:35:09 +0100

    nitter.net/theme: handle missing element.

--- nitter.net/theme.js
+++ nitter.net/theme.js
@@ -1,4 +1,5 @@
 let elt = document.querySelector('link[rel="stylesheet"][href*="/themes/"]');
-elt.href = "/css/themes/twitter.css";
+if (elt)
+    elt.href = "/css/themes/twitter.css";
 
 // vim:ts=4 sts=4 sw=4 et
